8ce9945313ad4b028ced2df5cb092b3398d404ac
Alex Gryzlov <gryzlovalex95@gmail.com>
2022-06-05 13:35:18 -0600

    NEED WORK

---
+++ include/ReceiptBag.h
@@ -0,0 +1,14 @@
+/*
+ File:   bag.h
+Author: Aleksandr Gryzlov
+Purpose:
+Implement the Bag receipt using bag.h
+*/
+
+#pragma once
+#include <iostream>
+#include "bag.h"
+
+class ReceiptBag {
+
+};
