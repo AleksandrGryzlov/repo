ae2eecb48bca559260723f34f3cb9bf3202eace0
Alex Gryzlov <gryzlovalex95@gmail.com>
2022-06-12 15:15:37 -0600

    GOOD

--- include/ReceiptBag.h
+++ include/ReceiptBag.h
@@ -7,10 +7,43 @@ Implement the Bag receipt using bag.h
 
 #pragma once
 #include <iostream>
-#include "bag.h"
+#include "list.h"
 
+template <typename Thing>
 class ReceiptBag {
 public:
+    bag() {
+        bagContents = new List<Thing*>;
+        bagContents[0] = nullptr;
+    }
+    ~bag() {
+        delete[] bagContents;
+        bagContents = nullptr;
+    }
+    void insert(Thing aThing) {
+        bagContents.push_back(&aThing);
+    }
+    void delet(Thing aThing) {
+        bagContents.delete(&aThing);
+    }
+    Thing& pop() {
+        Thing* ptrToAThing;
+        int i = bagContents.size();
+        bool thingIsNull = true;
+        while (thingIsNull && (i > 0)) {
+            if (bagContents[i] != nullptr) {
+                ptrToAThing = bagContents[i];
+                thingIsNull = false;
+            }
+        }
+    }
+    int size() {
+        return bagContents.capacity();
+    }
+    int count() {
+        return bagContents.size();
+    }
 private:
+    List<Thing*>* bagContents;
 	int receipt = 0;
 };
